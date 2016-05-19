70f4b5cae2484706b337398988cbb4f143ca7a51
Botezatu Mihai <Botezatu Mihai>
2016-05-19 21:55:36 +0300

    Adding more romanian stop words

--- src/core/RomanianAnalyzerWithoutDiacritics.java
+++ src/core/RomanianAnalyzerWithoutDiacritics.java
@@ -1,11 +1,5 @@
 package core;
 
-import java.io.IOException;
-import java.io.StringReader;
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.core.LowerCaseFilter;
@@ -15,7 +9,6 @@ import org.apache.lucene.analysis.ro.RomanianAnalyzer;
 import org.apache.lucene.analysis.snowball.SnowballFilter;
 import org.apache.lucene.analysis.standard.StandardFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
-import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.analysis.util.CharArraySet;
 import org.apache.lucene.analysis.util.StopwordAnalyzerBase;
 import org.tartarus.snowball.ext.RomanianStemmer;
@@ -34,59 +27,33 @@ public class RomanianAnalyzerWithoutDiacritics extends StopwordAnalyzerBase {
     protected TokenStreamComponents createComponents(String fieldName)
     {
         final Tokenizer source = new StandardTokenizer();
+        
+        this.getFullStopWordsSet();
 
         TokenStream tokenStream = source;
         tokenStream = new StandardFilter(tokenStream);
         tokenStream = new LowerCaseFilter(tokenStream);
-        tokenStream = new StopFilter(tokenStream, getStopwordSet());
-        tokenStream = new StopFilter(tokenStream, getStopWordsWithoutDiacritics());
+        tokenStream = new StopFilter(tokenStream, getFullStopWordsSet());
         tokenStream = new SnowballFilter(tokenStream, new RomanianStemmer()); // stemmer - flexionar forms (!)
         tokenStream = new ASCIIFoldingFilter(tokenStream); // replacing diacritics (!)
 
         return new TokenStreamComponents(source, tokenStream);
-    } 
-
-    public CharArraySet getStopWordsWithoutDiacritics()
-    {
-        String stopWordsWithoutDiacritics = getStopwordSet().toString();
-        try
-        {
-            stopWordsWithoutDiacritics = replaceDiacritics(stopWordsWithoutDiacritics);
-        }
-        catch(IOException e)
-        {
-            System.out.println("handled error");
-        }
-        
-        Set<String> setStopWordsWithoutDiacritics = Arrays.stream(stopWordsWithoutDiacritics.split(" ")).collect(Collectors.toSet());
-        CharArraySet stopWordsSet = new CharArraySet(setStopWordsWithoutDiacritics, false);
-        
-        return CharArraySet.unmodifiableSet(stopWordsSet);
     }
-
-    public static String replaceDiacritics(String textFile) throws IOException
-    {
-        TokenStream tokenStream = new StandardTokenizer();
-
-        ((Tokenizer) tokenStream).setReader(new StringReader(textFile.trim()));
-        tokenStream = new ASCIIFoldingFilter(tokenStream);
-
-        CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
-        tokenStream.reset();
-
-        StringBuilder sb = new StringBuilder();
-
-        while (tokenStream.incrementToken())
-        {
-            char[] term = charTermAttribute.toString().toCharArray();
-
-            ((ASCIIFoldingFilter) tokenStream).foldToASCII(term,term.length);
-            sb.append(term);
-            sb.append(" ");
-        }
-        
-        tokenStream.close();
-
-        return sb.toString();
+    
+    public CharArraySet getFullStopWordsSet() {
+    	
+    	CharArraySet stopWords = CharArraySet.copy(getStopwordSet());
+    	CharArraySet stopWordsWithoutDiacritics = getStopwordSet();
+    	java.util.Iterator<Object> iterator = stopWordsWithoutDiacritics.iterator();
+    	while (iterator.hasNext()) {
+    		 char[] stopWord = (char[]) iterator.next();
+    		 char[] output = new char[stopWord.length];
+    		 ASCIIFoldingFilter.foldToASCII(stopWord, 0, output, 0, stopWord.length);
+    		 stopWords.add(String.valueOf(output));
+    	}
+    	
+    	System.out.println(stopWords.toString());
+    	
+    	return stopWords;
     }
 }
--- src/core/RomanianStopWords.java
+++ src/core/RomanianStopWords.java
@@ -13,8 +13,13 @@ public class RomanianStopWords {
 	private static CharArraySet _stopWords = new CharArraySet(0, true);
 
 	static {
+		readIntoStopWords("stopwords_ro1.txt");
+		readIntoStopWords("stopwords_ro2.txt");
+	}
+	
+	static private void readIntoStopWords(String fileName) {
 		try (
-			InputStream inputStream = RomanianStopWords.class.getResourceAsStream("stopwords_ro.txt");
+			InputStream inputStream = RomanianStopWords.class.getResourceAsStream(fileName);
 			InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
 		    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 		) {
